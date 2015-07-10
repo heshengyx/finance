da987b90241616ac8b5a37733bae609bc7b578ee
hes <heshengyx@qq.com>
2015-07-10 22:32:22 +0800

    2

--- com/main/java/com/myself/finance/security/SecurityRealm.java
+++ com/main/java/com/myself/finance/security/SecurityRealm.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
@@ -39,21 +37,10 @@ public class SecurityRealm extends AuthorizingRealm {
 	@Autowired
 	private PermissionDao permissionDao;
 
-	/**
-	 * 为当前登录的Subject授予角色和权限
-	 * 
-	 * @see 经测试:本例中该方法的调用时机为需授权资源被访问时
-	 * @see 经测试:并且每次访问需授权资源时都会执行该方法中的逻辑,这表明本例中默认并未启用AuthorizationCache
-	 * @see 个人感觉若使用了Spring3
-	 *      .1开始提供的ConcurrentMapCache支持,则可灵活决定是否启用AuthorizationCache
-	 * @see 比如说这里从数据库获取权限信息时,先去访问Spring3.1提供的缓存,而不使用Shior提供的AuthorizationCache
-	 */
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(
 			PrincipalCollection principals) {
-		// 为当前用户设置角色和权限
 		SimpleAuthorizationInfo simpleAuthorInfo = null;
-		// 获取当前登录的用户名
 		/*String currentUsername = (String) super
 				.getAvailablePrincipal(principals);*/
 		User user = (User) super
@@ -64,7 +51,6 @@ public class SecurityRealm extends AuthorizingRealm {
 		//User user = userService.getByUserName(currentUsername);
 		if (null != user) {
 			List<Role> roles = roleDao.queryRolesByUserId(user.getId());
-			// 实体类User中包含有用户角色的实体类信息
 			if (!CollectionUtils.isEmpty(roles)) {
 				for (Role role : roles) {
 					roleList.add(role.getName());
@@ -85,19 +71,14 @@ public class SecurityRealm extends AuthorizingRealm {
 		return simpleAuthorInfo;
 	}
 
-	/**
-	 * 验证当前登录的Subject
-	 * 
-	 * @see 经测试:本例中该方法的调用时机为LoginController.login()方法中执行Subject.login()时
-	 */
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(
 			AuthenticationToken authcToken) throws AuthenticationException {
 		AuthenticationInfo authcInfo = null;
 		UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
-		System.out.println("验证当前Subject时获取到token为"
+/*		System.out.println("锟斤拷证锟斤拷前Subject时锟斤拷取锟斤拷token为"
 				+ ReflectionToStringBuilder.toString(token,
-						ToStringStyle.MULTI_LINE_STYLE));
+						ToStringStyle.MULTI_LINE_STYLE));*/
 		User user = userDao.getUserByAccount(token.getUsername());
 		if (null != user) {
 			authcInfo = new SimpleAuthenticationInfo(user,
@@ -111,8 +92,8 @@ public class SecurityRealm extends AuthorizingRealm {
 		Subject currentUser = SecurityUtils.getSubject();
 		if (null != currentUser) {
 			Session session = currentUser.getSession();
-			System.out
-					.println("Session默认超时时间为[" + session.getTimeout() + "]毫秒");
+			/*System.out
+					.println("Session默锟较筹拷时时锟斤拷为[" + session.getTimeout() + "]锟斤拷锟斤拷");*/
 			if (null != session) {
 				session.setAttribute(key, value);
 			}
