9533f5f765d4c81d048486a37d8bbc80a8547895
hes <heshengyx@qq.com>
2015-08-04 11:00:37 +0800

    0803

---
+++ src/main/java/com/myself/finance/controller/AccountController.java
@@ -0,0 +1,29 @@
+package com.myself.finance.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home/account")
+public class AccountController extends BaseController {
+
+	private final static Logger logger = LoggerFactory
+			.getLogger(AccountController.class);
+	
+	@RequestMapping("/resume")
+	public String resume() {
+		return "resume";
+	}
+	
+	@RequestMapping("/security")
+	public String security() {
+		return "security";
+	}
+	
+	@RequestMapping("/bankcard")
+	public String bankcard() {
+		return "bankcard";
+	}
+}
